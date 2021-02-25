d45dcfc8c7b6d0f18186cd9d79d75e8e2c02c1f8
Erica White <erica.white09@yahoo.com>
2021-02-25 15:33:33 -0700

    Created a duck of a geeky variety

---
+++ GeekyDuck.java
@@ -0,0 +1,13 @@
+class GeekyDuck extends Duck{
+
+    public GeekyDuck() {
+            quackBehavior = new Quack();
+        }
+
+
+    public void display() {
+            System.out.println("Geeky duck displayed");
+        }
+
+
+}
