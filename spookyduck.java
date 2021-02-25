2356b054846b39b0658d2a4b0ec6c3abe2cb6e11
hunter-holton <hunter.holton@me.com>
2021-02-25 15:07:39 -0700

    added in a spooky duck with normal quack behavior

---
+++ spookyduck.java
@@ -0,0 +1,12 @@
+class spookyduck extends Duck {
+
+    public GiantDuck() {
+      quackBehavior = new Quack();
+    }
+  
+    public void display() {
+      System.out.println("Spooky duck displayed ahhhhhhhh");
+    }
+  
+  }
+
